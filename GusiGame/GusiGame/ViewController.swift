1a1c11cf384352be07142424a58dafb11b959301
khomyakovdaniil <khomyakovdaniil@gmail.com>
2021-10-05 20:03:17 +0300

    Moveable cards
    added 4 images of syllables, and pangesture recognizer for them

---
+++ GusiGame/GusiGame/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  GusiGame
+//
+//  Created by  Даниил Хомяков on 02.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var initialCenter = CGPoint()  // The initial center point of the view.
+    @IBAction func panPiece(_ gestureRecognizer : UIPanGestureRecognizer) {
+       guard gestureRecognizer.view != nil else {return}
+       let piece = gestureRecognizer.view!
+       // Get the changes in the X and Y directions relative to
+       // the superview's coordinate space.
+       let translation = gestureRecognizer.translation(in: piece.superview)
+       if gestureRecognizer.state == .began {
+          // Save the view's original position.
+          self.initialCenter = piece.center
+       }
+          // Update the position for the .began, .changed, and .ended states
+       if gestureRecognizer.state != .cancelled {
+          // Add the X and Y translation to the view's original position.
+          let newCenter = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
+          piece.center = newCenter
+       }
+       else {
+          // On cancellation, return the piece to its original location.
+          piece.center = initialCenter
+       }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

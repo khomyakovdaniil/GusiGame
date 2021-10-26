c435ca43a91a7ba136c87df83e6dcccc9af93fc0
khomyakovdaniil <khomyakovdaniil@gmail.com>
2021-10-26 18:29:12 +0300

    All cards, DataManager, Xib
    Added all cards to the screen, added DataManager class to control resources, added Xib for custom UIView class representing syllable card

--- GusiGame/GusiGame/ViewController.swift
+++ GusiGame/GusiGame/ViewController.swift
@@ -9,43 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var gu1: UIView!
-    @IBOutlet weak var si2: UIView!
-    @IBOutlet weak var li3: UIView!
-    @IBOutlet weak var sa4: UIView! // Cards with syllables
+    @IBOutlet var leftSyllables: [SyllableView]!
+    @IBOutlet var rightSyllables: [SyllableView]!// Cards with syllables
     
     @IBOutlet weak var slot: UIView!
     @IBOutlet weak var slot2: UIView! // Slots
     
-    @IBOutlet weak var lisaImage: UIImageView!
-    @IBOutlet weak var gusiImage: UIImageView! // Images
+    @IBOutlet weak var resultImage: UIImageView! // Result Image
     
-    var originalCenterGu = CGPoint()
-    var originalCenterSi = CGPoint()
-    var originalCenterLi = CGPoint()
-    var originalCenterSa = CGPoint() // The original position of each card
+    var originalCenters = [CGPoint()]// The original position of each card
     
     var leftSlot = "" //Indicates which card is in the left slot
     var rightSlot = "" //Indicates which card is in the right slot
     
     var initialCenter = CGPoint()  // The initial center point of the view for movement
     
+    let words = DataManager().getList()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        originalCenterGu = gu1.center
-        originalCenterSi = si2.center
-        originalCenterLi = li3.center
-        originalCenterSa = sa4.center // Saves the original position of cards
-        lisaImage.isHidden = true
-        gusiImage.isHidden = true
+        var lSsetupCounter = 0
+        for syllable in leftSyllables {
+            syllable.setup(text: words[lSsetupCounter].leftSyllable, leftSyllable: true)
+            originalCenters.append(syllable.center)
+            lSsetupCounter += 1
+        }
+        
+        var rSsetupCounter = 0
+        for syllable in rightSyllables {
+            syllable.setup(text: words[rSsetupCounter].rightSyllable, leftSyllable: false)
+            originalCenters.append(syllable.center)
+            rSsetupCounter += 1
+        }
+        
+        resultImage.isHidden = true
     }
     
     @IBAction func panPiece(_ gestureRecognizer : UIPanGestureRecognizer){
         guard gestureRecognizer.view != nil else {return}
         
-        let piece = gestureRecognizer.view!
+        let piece = gestureRecognizer.view! as! SyllableView
+        
         let translation = gestureRecognizer.translation(in: piece.superview)
         
         if gestureRecognizer.state == .began {
@@ -59,25 +64,11 @@ class ViewController: UIViewController {
             piece.center = newCenter
             // Add the X and Y translation to the view's original position.
             
-            func cardText() -> String{
-                switch piece.tag{
-                case 1:
-                    return "Gu"
-                case 2:
-                    return "Si"
-                case 3:
-                    return "Li"
-                case 4:
-                    return "Sa"
-                default:
-                    return "none"
-                }
-            } // Indicates which syllable is in the current moving card
             if abs(piece.center.x-slot.center.x)<50 && abs(piece.center.y-slot.center.y)<50{
-                leftSlot = cardText()
+                leftSlot = piece.title.text!
             }
             if abs(piece.center.x-slot2.center.x)<50 && abs(piece.center.y-slot2.center.y)<50{
-                rightSlot = cardText()
+                rightSlot = piece.title.text!
             } //Saves cards syllable to the slot
         }
         else {
@@ -85,48 +76,37 @@ class ViewController: UIViewController {
         } // On cancellation, return card to its initial location.
         
         if gestureRecognizer.state == .ended {
-            func originalCenter() -> CGPoint {
-                switch piece.tag{
-                case 1:
-                    return originalCenterGu
-                case 2:
-                    return originalCenterSi
-                case 3:
-                    return originalCenterLi
-                case 4:
-                    return originalCenterSa
-                default:
-                    return originalCenterGu
-                }
-            }
             if ((abs(piece.center.x-slot.center.x)<50 && abs(piece.center.y-slot.center.y)<50) || (abs(piece.center.x-slot2.center.x)<50 && abs(piece.center.y-slot2.center.y)<50)) == false {
-                piece.center = originalCenter()
+                piece.center = originalCenters[piece.tag]
             } // On the end of movement return the card to its original location, if it was not moved into the slot
         }
         
     }
     @IBAction func CheckButton(_ sender: UIButton) { //Checks the slots, displays image, destroys cards
-        switch leftSlot + rightSlot {
-        case "GuSi":
-            gusiImage.isHidden = false
-            leftSlot = ""
-            rightSlot = ""
-            gu1.removeFromSuperview()
-            si2.removeFromSuperview()
-            
-            
-        case "LiSa":
-            lisaImage.isHidden = false
-            leftSlot = ""
-            rightSlot = ""
-            li3.removeFromSuperview()
-            sa4.removeFromSuperview()
-            
-        default:
-            sender.setTitle("Неправильно", for: .normal)
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                sender.setTitle("Проверить", for: .normal)
+        
+        var checkCounter = 1
+        for word in words {
+            checkCounter += 1
+            if leftSlot + rightSlot == word.fullWord {
+                self.resultImage.image = word.image
+                self.resultImage.isHidden = false
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    self.resultImage.isHidden = true
+                }
+                leftSlot = ""
+                rightSlot = ""
+                let indexOfWord = words.firstIndex(of: word)!
+                leftSyllables[indexOfWord].removeFromSuperview()
+                rightSyllables[indexOfWord].removeFromSuperview()
+                checkCounter = 0
             }
+            if checkCounter > 9 {
+                sender.setTitle("Неправильно", for: .normal)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    sender.setTitle("Проверить", for: .normal)
+                }
+            }
+
         }
     }
 }
